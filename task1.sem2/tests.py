4333b71a5bf7fcff12e88f9572cc7ba2e5aec9e5
Anastasiia <116254940+feuerstrahll@users.noreply.github.com>
2023-01-17 20:04:53 +0300

    Update tests.py

--- task1.sem2/tests.py
+++ task1.sem2/tests.py
@@ -8,7 +8,7 @@ class MyTestCase(unittest.TestCase):
                 '2. Бакушкин Даниил: 3\n'
                 '3. Шахвалиева Юлиана: 4')
         result = pre(test)
-        self.assertEqual((['Котов Алексей: 3', 'Бакаушин Даниил: 3', 'Шахвалиева Юлиана: 4']), result)
+        self.assertEqual((['Бакаушин Даниил, 3', 'Котов Алексей, 3', 'Шахвалиева Юлиана, 4']), result)
         
 if __name__ == '__main__':
     unittest.main()
