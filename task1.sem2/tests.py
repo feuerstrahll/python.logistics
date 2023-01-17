23b95f186ff8ea93338ca216f0195f61850b57a7
Anastasiia <116254940+feuerstrahll@users.noreply.github.com>
2023-01-17 19:20:02 +0300

    Create tests.py

---
+++ task1.sem2/tests.py
@@ -0,0 +1,14 @@
+import unittest
+from main.py import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test_pre(self):
+        test = ('1. Котов Алексей: 3\n'
+                '2. Бакушкин Даниил: 3\n'
+                '3. Шахвалиева Юлиана: 4')
+        result = pre(test)
+        self.assertEqual(result, (['Котов Алексей: 3', 'Бакаушин Даниил: 3', 'Шахвалиева Юлиана: 4']))
+        
+if __name__ == '__main__':
+    unittest.main()
