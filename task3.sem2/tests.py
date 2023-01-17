7c3321c4d796f5bef7b44d948c493fa57a8f7a23
Anastasiia <116254940+feuerstrahll@users.noreply.github.com>
2023-01-17 21:36:48 +0300

    Create tests.py

---
+++ task3.sem2/tests.py
@@ -0,0 +1,17 @@
+import unittest
+from main import formatted_file, read_file
+
+
+class Test_task1(unittest.TestCase):
+    def test_pre(self):
+        
+        self.assertEqual(read_file("data.txt")[:2], ['She', 'married'])
+        
+        prepare = read_file("data.txt")
+        self.assertEqual(formatted_file(prepare),   'She married a very\n'
+                                                    'nice young architect\n'
+                                                    'from Belfast, whom\n'
+                                                    'she met on a bus. ')
+        
+if __name__ == '__main__':
+    unittest.main()
