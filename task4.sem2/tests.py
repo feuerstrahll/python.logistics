2b6b0720107c8192fb9c07f3063b61ede6c83fa5
Anastasiia <116254940+feuerstrahll@users.noreply.github.com>
2023-01-18 01:44:23 +0300

    Update tests.py

--- task4.sem2/tests.py
+++ task4.sem2/tests.py
@@ -1,1 +1,11 @@
-b
+import unittest
+from tests import prepare, read_file
+
+
+class Test_task4(unittest.TestCase):
+    def test(self):
+        rd = read_file("input.txt")
+        self.assertEqual(prepare(rd)[:rd.index(" ")+5], "('i', 3)" )
+        
+if __name__ == '__main__':
+    unittest.main()
