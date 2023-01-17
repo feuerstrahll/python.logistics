c35f7992a9dddafdd06e4c907c51044eb3b26385
Anastasiia <116254940+feuerstrahll@users.noreply.github.com>
2023-01-18 01:52:20 +0300

    Update tests.py

--- task2.sem2/tests.py
+++ task2.sem2/tests.py
@@ -5,11 +5,7 @@ from data import sort_and_out, read_file
 class Test_task2(unittest.TestCase):
     def test(self):
         rf = read_file("input.txt")
-        self.assertEqual(sort_and_out(rf), 'KP2.2-01 - Vedomost\n'
-                                                    'KP2.2-01.1 - Vedomost\n'
-                                                    'KP2.2-01.2 - Vedomost\n'
-                                                    'KP2.2-01.3 - Vedomost\n'
-                                                    'KP2.2-2 - Schema\n')
+        self.assertEqual(sort_and_out(rf), read_file("output.txt"))
         
 if __name__ == '__main__':
     unittest.main()
