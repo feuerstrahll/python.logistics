3f8f8991a1a2691ebaada99b3398c25456d505a8
Anastasiia <116254940+feuerstrahll@users.noreply.github.com>
2023-01-19 22:57:17 +0300

    Update tests.py

--- task5.sem2/tests.py
+++ task5.sem2/tests.py
@@ -1,1 +1,30 @@
-e
+import unittest
+from tests import formatted_to_anagrams
+
+
+class Test_task5(unittest.TestCase):
+    def test(self):
+        desired = [['ate', 'eat', 'tea'], ['bat'], ['nat', 'tan']]
+        data = ["eat","tea","tan","ate","nat","bat"]
+        self.assertEqual(formatted_to_anagrams(data), desired)
+        
+if __name__ == '__main__':
+    unittest.main()
+    
+
+--------------------------------------------------------------
+
+
+
+import unittest
+from tests import formatted_to_anagrams
+
+
+class Test_task5(unittest.TestCase):
+    def test(self):
+        desired = [['bar', 'bra', 'rab'], ['com', 'moc'], ['llo', 'lol', 'oll']]
+        data = ["bra","bar","com","rab","lol","moc", "llo", "oll"]
+        self.assertEqual(formatted_to_anagrams(data), desired)
+        
+if __name__ == '__main__':
+    unittest.main()
