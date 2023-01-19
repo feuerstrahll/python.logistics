fc4ee1d4ca4df8e9991cb71b8d57b3666f016916
Anastasiia <116254940+feuerstrahll@users.noreply.github.com>
2023-01-19 22:48:23 +0300

    Update main.py

--- task5.sem2/main.py
+++ task5.sem2/main.py
@@ -1,1 +1,16 @@
-r
+def formatted_to_anagrams(data):
+    dict_f_a = {}
+    list_f_a = []
+    for word in data:
+        s_word = ''.join(sorted(word))
+        if s_word in dict_f_a:
+	        dict_f_a[s_word].append(word)
+        else:
+	        dict_f_a[s_word] = [word]
+    for w in list(dict_f_a.values()):
+        list_f_a.append(sorted(w))
+    return list_f_a
+
+words = ["eat","tea","tan","ate","nat","bat"]
+print(formatted_to_anagrams(words))
+
