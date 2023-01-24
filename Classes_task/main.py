3102509595cafb0486a421b24415857e1dcae978
Anastasiia <116254940+feuerstrahll@users.noreply.github.com>
2023-01-24 19:59:27 +0300

    Update main.py

--- Classes_task/main.py
+++ Classes_task/main.py
@@ -12,24 +12,7 @@ class User:
     
     def add_user(username):
         storage.add_user(User(username = username, cr_time = datetime.now().strftime('%D.%M.%Y')))
-        
-class UserStorage:
-    id_generator = 1
-    def __init__(self):
-        self.__RECORDS = {}
-        
-    def get_user(self, id):
-        return self.__RECORDS.get(id, None)
-        
-    def add_user(self, user):
-        gloal id_generator
-        user.set_id(id_generator)
-        self.__RECORDS[id_generator] = user
-        id_generator += 1 
-        return user.id
-
-storage = Storage()
-user_storage = UserStorage()
+       
 
 class Chat:
     def __init__(self, id, name, users, cr_time, storage):
@@ -44,25 +27,7 @@ class Chat:
         storage.add_chat(Chat(name = name, users = users, cr_time = datetime.now().strftime('%D.%M.%Y')))
 
 
-class ChatStorage:
-    c_id_generator = 1
-    def __init__(self):
-        self.__RECORDS = {}
-        
-    def get_chat(self, id):
-        return self.__RECORDS.get(id, None)
-        
-    def add_chat(self, chat):
-        global c_id_generator
-        chat.set_id(id_generator)
-        self.__RECORDS[id_generator]= chat
-        c_id_generator += 1
-        for user in chat.users:
-            user_storage.get_user(user).add_chat(chat)
-        return chat.id
-        
-chat_storage = ChatStorage()
-        
+
 class Message:
     m_id_generator = 1
     def __init__(self, id, chat_id, user_id, text, cr_time):
@@ -88,33 +53,5 @@ class Message:
         return message.id
     
     
-class MessageStorage:
-    m_id_generator = 1
-    def __init__(self):
-        self.__RECORDS = {}
-        
-    def get_message(self, id):
-        return self.__RECORDS.get(id, None)
-    
-    def add_message(self, message):
-        global m_id_generator
-        m_id_generator += 1 
-        message.set_id(m_id_generator)
-        self.__RECORDS[m_id_generator] = message
-        self.__RECORDS[message.id] = message
-        m_id_generator += 1
-        
-message_storage = Message()
         
         
-def add_user(username):
-    user = User(username)
-    return(user_storage.add_user(user))
-
-def add_chat(name, users):
-    chat = Chat(name, users)
-    return(chat_storage.add_chat(chat))
-        
-def add_message(chat_id, user_id, text):
-    add_message = Message(chat_id, user_id, text)
-    return(message_storage.add_message(add_message))   
