0de0bdee8576dba0fefe033ecf568ccfec19cdad
Anastasiia <116254940+feuerstrahll@users.noreply.github.com>
2023-01-24 11:47:36 +0300

    Create Storages.py

---
+++ Classes_task/Storages.py
@@ -0,0 +1,50 @@
+class UserStorage:
+    id_generator = 1
+    def __init__(self):
+        self.__RECORDS = {}
+        
+    def get_user(self, id):
+        return self.__RECORDS.get(id, None)
+        
+    def add_user(self, user):
+        gloal id_generator
+        user.set_id(id_generator)
+        self.__RECORDS[id_generator] = user
+        id_generator += 1 
+        return user.id
+
+
+class ChatStorage:
+    c_id_generator = 1
+    def __init__(self):
+        self.__RECORDS = {}
+        
+    def get_chat(self, id):
+        return self.__RECORDS.get(id, None)
+        
+    def add_chat(self, chat):
+        global c_id_generator
+        chat.set_id(id_generator)
+        self.__RECORDS[id_generator]= chat
+        id_generator += 1
+        for user in chat.users:
+            user_storage.get_user(user).add_new_chat(chat)
+        return chat.id
+                
+class MessageStorage:
+    m_id_generator = 1
+    def __init__(self):
+        self.__RECORDS = {}
+        
+    def get_message(self, id):
+        return self.__RECORDS.get(id, None)
+    
+    def add_message(self, message):
+        global m_id_generator
+        m_id_generator += 1 
+        message.set_id(m_id_generator)
+        self.__RECORDS[m_id_generator] = message
+        self.__RECORDS[message.id] = message
+        m_id_generator += 1
+        return message.id
+        
